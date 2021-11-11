6882c09a6e0154096890fff895ea26240f73fc5e
Liza Vasina <liz.vasina@gmail.com>
2021-11-11 13:16:15 +0300

    Переписать videos на функц компоненты и создать хук useVideos

---
+++ videos-hooks/src/hooks/useVideos.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+import youtube from "../apis/youtube";
+
+const useVideos = (defaultSearchTerm) => {
+    const [videos, setVideos] = useState([]);
+
+    useEffect(() => {
+        search(defaultSearchTerm);
+    }, [defaultSearchTerm]);
+
+    const search = (term) => {
+        youtube.get('/search', {
+            params: {
+                "q": term,
+            }
+        })
+        .then(res => {
+            setVideos(res.data.items);
+            
+        })
+        .catch(err  => console.log(err));
+    }
+
+    return [videos, search];
+}
+
+export default useVideos;
